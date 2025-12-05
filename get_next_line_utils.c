2a061b40297f98cba889b0a73a3f8c8a395ec31b
Itakaah <am7.usmanov@gmail.com>
2025-12-05 21:37:33 +0100

    Added the functions strlen, substr, strchr and strjoin to have all the utils functions for the rest of the projet

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -2,83 +2,73 @@
 
 size_t	ft_strlen(const char *s)
 {
-	size_t	i;
+	size_t	len;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	if (!s)
-		return (NULL);
-	i = 0;
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
-		i++;
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
 	}
-	if (s[i] == (char)c)
-		return ((char *)&s[i]);
+	if ((char)c == '\0')
+		return ((char *)s);
 	return (NULL);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*result;
-	size_t	i;
-	size_t	j;
-
-	if (!s1)
-	{
-		s1 = malloc(1);
-		if (!s1)
-			return (NULL);
-		s1[0] = '\0';
-	}
-	if (!s2)
-		return (NULL);
-	result = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!result)
-		return (NULL);
-	i = -1;
-	while (s1[++i])
-		result[i] = s1[i];
-	j = 0;
-	while (s2[j])
-		result[i++] = s2[j++];
-	result[i] = '\0';
-	free(s1);
-	return (result);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*substr;
 	size_t	i;
 	size_t	s_len;
+	char	*res;
 
 	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		return (malloc(1));
+		return (ft_strdup(""));
 	if (len > s_len - start)
 		len = s_len - start;
-	substr = malloc(sizeof(char) * (len + 1));
-	if (!substr)
+	res = (char *)malloc(len + 1);
+	if (!res)
 		return (NULL);
 	i = 0;
 	while (i < len && s[start + i])
 	{
-		substr[i] = s[start + i];
+		res[i] = s[start + i];
 		i++;
 	}
-	substr[i] = '\0';
-	return (substr);
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (NULL);
+	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
 }
